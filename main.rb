aee9837661fe112a1e4fb87da563257caf606c27
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-07-08 13:40:06 -0600

    add i/o console to avoid showing master secret selection in console, add method to display instruction of choosing colors, being separate from the loop it avoids flooding the console with the same message

--- main.rb
+++ main.rb
@@ -1,16 +1,18 @@
+require 'io/console'
+
 module Mastermind
   class Game
     attr_reader :hidden_combination, :colors
 
     def initialize
-      @hidden_combination = [] 
+      @master_combination = [] 
       @colors = ['red', 'blue', 'orange', 'yellow', 'green', 'purple']
       @master = Master.new(self)
       @hacker = Hacker.new(self)
     end
 
-    def add_hidden_color!(color)
-      @hidden_combination.push(color)
+    def add_master_color!(color)
+      @master_combination.push(color)
     end
 
     def play
@@ -27,10 +29,14 @@ module Mastermind
     end
     attr_reader :chosen_color
 
+    def color_to_choose
+      puts 'choose 4 colors from 1-6, input one number at a time and then click enter', @game.colors
+    end
+
+
     def choose_color!
-      puts 'choose 1 color from 1-6', @game.colors
-      color_index = gets.to_i - 1
-      @chosen_color = @game.colors[color_index] if right_input?(color_index)
+      color_index = STDIN.noecho(&:gets)
+      @chosen_color = @game.colors[color_index.to_i] if right_input?(color_index.to_i)
     end
 
     def right_input?(index)
@@ -45,9 +51,10 @@ module Mastermind
 
   class Master < Player
     def master_selects
+      color_to_choose
       4.times do 
         choose_color!
-        @game.add_hidden_color!(@chosen_color)
+        @game.add_master_color!(@chosen_color)
         @chosen_color = ''
       end
     end
