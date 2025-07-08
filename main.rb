5e7ea6456572458e900121c1aa3ff02515ece9d5
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-07-07 19:25:31 -0600

    add colors variable to game

--- main.rb
+++ main.rb
@@ -1,9 +1,12 @@
 module Mastermind
   class Game
     attr_reader :hidden_combination
+
     def initialize
       @hidden_combination = Array.new(4) {' '} 
+      @colors = ['red', 'blue', 'orange', 'yellow', 'green', 'purple']
     end
+
   end
 end
 
