f67450ceb173e286f19a47d5e17d6f4e91b2fbef
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-07-07 19:55:08 -0600

    add chosen color variable, add choose_color method to get the user input for choosing a color from 1-6, reducing the chosen number by 1 to work with the index

--- main.rb
+++ main.rb
@@ -1,10 +1,17 @@
 module Mastermind
   class Game
-    attr_reader :hidden_combination
+    attr_reader :hidden_combination, :chosen_color
 
     def initialize
       @hidden_combination = Array.new(4) {' '} 
       @colors = ['red', 'blue', 'orange', 'yellow', 'green', 'purple']
+      @chosen_color = ''
+    end
+
+    def choose_color
+      puts 'choose 1 color from 1-6', @colors
+      color_index = gets.to_i - 1
+      @chosen_color = @colors[color_index]
     end
 
   end
@@ -13,4 +20,5 @@ end
 include Mastermind
 
 game = Game.new
-p game.hidden_combination
+game.choose_color
+p game.chosen_color
